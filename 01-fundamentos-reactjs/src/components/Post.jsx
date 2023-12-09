67313734f342c177e2b00128bbde81d4fd28471a
Jheison Rocha <jheisonr10@gmail.com>
2023-12-09 18:37:54 -0300

    Estilizando o Post

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -15,15 +15,20 @@ export function Post() {
             <time title="08 de dezembro ás 00:17h"  dateTime="2023-12-08 00:17:50">Publicado há 1h   </time>
          </header>
 
-         <div className={styles.content}></div>
+         <div className={styles.content}>
         
             <p>Fala galeraa 👋</p>
 
             <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-            <p>👉 <a href="">jane.design/doctorcare</a></p>
+            <p><a href="">jane.design/doctorcare</a></p>
 
-            <p> <a href="">#novoprojeto #nlw #rocketseat</a></p>
+            <p> 
+                <a href="">#novoprojeto</a>{' '}
+                <a href="">#nlw</a>{' '}
+                <a href="">#rocketseat</a>
+             </p> 
+            </div>
         
     </article>
     )
