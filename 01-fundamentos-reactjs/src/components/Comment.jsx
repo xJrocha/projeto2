4b3c7c7cb039e044b6739c3c510ac7fc6c13bf05
Jheison Rocha <jheisonr10@gmail.com>
2023-12-14 01:06:32 -0300

    Estado(useStates)//Inserindo Comentários (Prog. Declarativa)

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 import{ThumbsUp, Trash} from '@phosphor-icons/react'
 
-export function Comment() {
+export function Comment({ content }) {
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/maykbrito.png" alt='' />
@@ -19,7 +19,7 @@ export function Comment() {
                     </button>
                 </header>
 
-                <p>Muito bom Devon, parabéns!! 👏👏</p>
+                <p>{content}</p>
 
             </div>
            
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -4,8 +4,16 @@ import ptBR from 'date-fns/locale/pt-BR'
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from "./Post.module.css";
+import { useState } from 'react';
 
 export function Post({ author , publishedAt , content  }) {
+    const [comments,setComments] = useState([
+        'Post muito bacana,hein?!'
+       ])
+
+       const [newCommentText, setNewCommentText] = useState('')
+
+
     const publishedDateFormatted = format(publishedAt,"d 'de' LLLL 'ás' HH:mm'h'", {
         locale:ptBR,
     })
@@ -14,6 +22,18 @@ export function Post({ author , publishedAt , content  }) {
         locale:ptBR,
         addSuffix: true,
     })
+
+    function handleCreateNewComment () {
+        event.preventDefault()
+
+        
+        setComments([...comments, newCommentText ]);
+        setNewCommentText('');
+    }
+
+    function handleNewCommentChange(){
+        setNewCommentText(event.target.value);
+    }
     
     return (
         <article className={styles.post}>
@@ -43,21 +63,26 @@ export function Post({ author , publishedAt , content  }) {
             
          </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
             <strong>Deixe seu feedback</strong>
 
-            <textarea 
-                placeholder="Deixe um comentário"
-            />    
+            <textarea
+                name="comment"
+                placeholder="Deixe seu comentário"
+                value={newCommentText}
+                onChange={handleNewCommentChange}
+            />
+
+           
 
                <footer>
                <button type="submit">Publicar</button>
                </footer>
             </form>
             <div className={styles.commentList}>
-              <Comment />
-              <Comment />
-              <Comment />
+            {comments.map(comment => {
+                return <Comment content={comment} />
+             })}
             </div>
             
                
