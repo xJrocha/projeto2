406944d8a60b97d9a3fd5c528432f94934a6cff1
Jheison Rocha <jheisonr10@gmail.com>
2023-12-24 02:12:54 -0300

    Adicionando TypeScript

---
+++ 01-fundamentos-reactjs-ts/src/components/Comment.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+import{ThumbsUp, Trash} from '@phosphor-icons/react'
+
+interface CommentProps {
+    content:string;
+    onDeleteComment:(comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+    const [LikeCount,setLikeCount] = useState (0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });            
+    }
+
+    return(
+        <div className={styles.comment}>
+            <Avatar 
+            hasBorder={false} 
+            src="https://github.com/maykbrito.png" 
+            alt='' 
+        />
+
+            <div className={styles.commentBox}>
+            <div className={styles.commentContent}>
+                <header>
+                    <div className={styles.authorandTime}>
+                        <strong>Jheison Rocha</strong>
+                        <time title="08 de dezembro ás 00:17h"  dateTime="2023-12-08 00:17:50">Cerca de 1 hora atrás</time>
+                    </div>
+                    <button onClick={handleDeleteComment}title="Deletar comentário">
+                        <Trash size={24}/>
+                    </button>
+                </header>
+
+                <p>{content}</p>
+
+            </div>
+           
+            <footer>
+                <button onClick={handleLikeComment}>
+                    <ThumbsUp />
+                    Aplaudir <span>{LikeCount}</span>
+                </button>
+            </footer>
+         </div>
+    </div>
+    )
+}
---
+++ 01-fundamentos-reactjs-ts/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import styles from'./Header.module.css'
+
+import igniteLogo from '../assets/ignite-simbol.svg';
+
+export function Header(){
+    return (
+        <header className={styles.header}>
+            <img src={igniteLogo} alt="logotipo do ignite" />
+         </header>
+     );
+
+}
